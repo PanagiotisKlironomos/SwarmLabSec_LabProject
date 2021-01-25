b7842a0ab66dda40e7768dfa4122cd20769317d8
PanagiotisKlironomos <73659518+PanagiotisKlironomos@users.noreply.github.com>
2021-01-25 13:12:40 +0200

    Update worker1.sh

--- 2)SSH_Brute_Force_Attack/worker1.sh
+++ 2)SSH_Brute_Force_Attack/worker1.sh
@@ -3,8 +3,8 @@ sudo apt upgrade -y
 sudo apt-get install fail2ban -y 
 sudo apt install rsyslog -y
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local 
-sudo cp "/project/ssh brute force attack/fail2bansshconf" /etc/fail2ban/jail.local 
-sudo cp "/project/ssh brute force attack/sshdconf" /etc/ssh/sshd_config 
+sudo cp "/project/2)SSH_Brute_Force_Attack/fail2bansshconf" /etc/fail2ban/jail.local 
+sudo cp "/project/2)SSH_Brute_Force_Attack/sshdconf" /etc/ssh/sshd_config 
 sudo service ssh restart
 sudo service rsyslog restart
 sudo service fail2ban restart
